7daf89d0aab96adc48a70a8c666a038abb285da4
CristianBarahona <htreal2010@hotmail.com>
2023-03-03 11:32:19 -0500

    Testing New database

--- src/main/java/com/cristianabv1993/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/cristianabv1993/contentcalendar/repository/ContentCollectionRepository.java
@@ -14,29 +14,45 @@ import java.util.Optional;
 @Repository
 public class ContentCollectionRepository {
 
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository(){
 
     }
 
     public List<Content> findAll(){
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id){
-        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+
+
+    public void save(Content content) {
+     contentList.removeIf(c-> c.id().equals(content.id()));
+    contentList.add(content);
     }
 
     @PostConstruct
     private void init(){
-        Content c = new Content(
+        Content content = new Content(
                 1,"My first blog post","My first blog post", Status.IDEA, Types.ARTICLE,
                 LocalDateTime.now(),
                 null,
                 ""
         );
 
-        content.add(c);
+        contentList.add(content);
+    }
+
+
+    public boolean existById(Integer id) {
+        return contentList.stream().filter(c -> c.id().equals(id)).count() == 1;
+    }
+
+    public void delete(Integer id) {
+        contentList.removeIf(c-> c.id().equals(id));
     }
 }
---
+++ src/main/java/com/cristianabv1993/contentcalendar/repository/ContentJdbcTemplateRepository.java
@@ -0,0 +1,5 @@
+package com.cristianabv1993.contentcalendar.repository;
+
+public class ContentJdbcTemplateRepository {
+
+}
